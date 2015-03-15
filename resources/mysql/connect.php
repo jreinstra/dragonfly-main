5559940092e8f9e35d163917ec1dc5846518147e
yohanstra <jreinstra@gmail.com>
2015-03-15 11:44:35 -0700

    Updated mysql connect for hosted settings

--- resources/mysql/connect.php
+++ resources/mysql/connect.php
@@ -1,10 +1,17 @@
 <?php
 
 //old hosted database settings
-$dbhost = localhost;
+/*$dbhost = localhost;
 $dbuser = "root";//beam_root
 $dbpass = "BEAM123";//Rx4sS4KJwDVEMGZc
-$dbname = "beam";
+$dbname = "beam";*/
+
+//i)8aiYsbXHeGxox[P(TdLZkG*BJm9LJGzZD9tkbWmC8%y2X^tt
+
+$dbhost = localhost;
+$dbuser = "beam";//beam_root
+$dbpass = "i)8aiYsbXHeGxox[P(TdLZkG*BJm9LJGzZD9tkbWmC8%y2X^tt";//Rx4sS4KJwDVEMGZc
+$dbname = "dragonfly";
 
 $con = mysqli_connect($dbhost, $dbuser, $dbpass);
 if(mysqli_connect_errno($con)) {
