58f2a4f238d1b829f228f410602c458b3f2a7e5f
Alex Fine <alexkfine2@gmail.com>
2015-03-16 04:08:44 +0000

    email.php created online with Bitbucket

---
+++ email.php
@@ -0,0 +1,87 @@
+<?php
+
+if(isset($_POST['email'])) {
+     
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+   
+     
+     
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+     
+    // validation expected data exists
+    if( !isset($_POST['email']) ||
+        !isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+     
+    $email_from = $_POST['email']; // required
+    $comments = $_POST['comments']; // required
+     
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(strlen($comments) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+     
+ 
+    $email_message .= "Your Notes: ".clean_string($comments)."\n"."\n"."You have recived this message because you reqested your notes to be sent to you from dragonflysearch.com";
+     
+      $email_to = $email_from;
+    $email_subject = "Your Notes";
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+
+  <meta http-equiv="refresh" content="0; url=/search/?q=<?php echo $subject; ?>" />
+ <script type="text/javascript">
+            window.location.href = "http://dragonflysearch.com/search/?q=<?php echo $subject; ?>"
+        </script>
+ </head>
+ 
+ <body>
+<script>
+ alert("You're notes have been sent! Check your spam folder if you can't find them in a few minutes.")
+</script>
+ </body>
+</html>
+<?php
+}
+?>
+
+ <body>
+<script>
+ alert("You're notes have been sent! Check your spam folder if you can't find them in a few minutes.")
+</script>
+ </body>
+</html>
+<?php
+}
+?>
