9025bd643fd2c1c28ce056622e21d72527739e7e
Alex Fine <alexkfine2@gmail.com>
2015-03-16 04:29:31 +0000

    email.php edited online with Bitbucket

--- email.php
+++ email.php
@@ -62,15 +62,15 @@ $headers = 'From: '.$email_from."\r\n".
 <script>
  alert("You're notes have been sent! Check your spam folder if you can't find them in a few minutes.");
 </script>
-  <meta http-equiv="refresh" content="0; url=/search/?q=paint" />
+  <meta http-equiv="refresh" content="0; url=/index.php" />
  <script type="text/javascript">
-            window.location.href = "http://dragonflysearch.com/search/?q=paint"
+            window.location.href = "http://dragonflysearch.com/"
         </script>
  </head>
  
  <body>
 <script>
-    alert("Hello! I am an alert box!");
+    alert("You're notes have been saved! Expect an email shortly. If you don't see it, check your spam folder.");
 
 </script>
 
