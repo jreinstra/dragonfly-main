3822487220788fc98d1ce613bf37b20a4c84b539
Alex Fine <alexkfine2@gmail.com>
2015-03-16 17:14:36 +0000

    index.php edited online with Bitbucket

--- search/index.php
+++ search/index.php
@@ -223,7 +223,6 @@ Videos coming April 1st!
 </div>
 </form>
 <!-- End Email Form -->    
-      <div id="carbonads-container"><div class="carbonad" style="width:100%;"><div id="azcarbon"></div><script>var z = document.createElement("script"); z.async = true; z.src = "http://engine.carbonads.com/z/32341/azcarbon_2_1_0_HORIZ"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(z, s);</script></div></div>
 
   </div>
 
