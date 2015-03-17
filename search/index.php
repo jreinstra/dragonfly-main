74d5ab1d07f809fde3583f204019bcc21c6c461e
yohanstra <jreinstra@gmail.com>
2015-03-16 22:05:10 -0700

    Bug fix

--- search/index.php
+++ search/index.php
@@ -5,8 +5,8 @@ if(!isset($q) || strlen($q) < 1) {
 	die();
 }
 $q = strtolower($q);
-$results = file_get_contents("http://" . $_SERVER["HTTP_HOST"] . "/api/suggest.php?q=" . urlencode($q), true);
-$results = file_get_contents("http://" . $_SERVER["HTTP_HOST"] . "/api/search.php?q=" . urlencode($q), true);
+$results = file_get_contents("http://" . $_SERVER["HTTP_HOST"] . "/api/suggest.php?q=" . urlencode($q));
+$results = file_get_contents("http://" . $_SERVER["HTTP_HOST"] . "/api/search.php?q=" . urlencode($q));
 $results = json_decode($results, true);
 
 $subject = $results["Subject"];
