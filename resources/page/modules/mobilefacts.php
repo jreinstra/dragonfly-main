291acd15a4b879ccf8aee3512fa209875f37b9cb
Alex Fine <alexkfine2@gmail.com>
2015-03-18 00:27:03 +0000

    Mobile Facts

---
+++ resources/page/modules/mobilefacts.php
@@ -0,0 +1,6 @@
+<blockquote>
+        <p><strong>  <?php echo $i; ?>. </strong><?php echo $fact; ?> 
+                   <small>en.wikipedia.org/<?php echo $subject; ?><cite title="Source Title"></cite></small>
+
+  </blockquote>
+  
