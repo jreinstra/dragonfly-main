00bfbc42b505ac7f70b665a79dc94d863bb08b3e
Jackson Vachal <vacsox@yahoo.com>
2015-04-08 04:27:04 +0000

    Getting rid of the expand glyph

--- resources/page/modules/factsbeta2.php
+++ resources/page/modules/factsbeta2.php
@@ -57,7 +57,6 @@ $sentence = preg_split('/\(?[^\.\?\!]+[\.!\?]\)?/g',$fact);
 <div><div class="btn-group btn-group-sm" role="group" aria-label="...">
   <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_343_thumbs_up.png" style="width:18px; height:18px;"> </a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
     <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
- <button type="button" class="btn btn-group-sm btn-default"> <img src="http://beam.la/glyphicons-187-move.png" style="width:18px; height:18px;"></button>
  <!-- Large modal -->
 
 
