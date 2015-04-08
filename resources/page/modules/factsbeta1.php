b1e4f81b962e6385da15bed9b3fdec8568fd60cb
Jackson Vachal <vacsox@yahoo.com>
2015-04-08 01:42:03 +0000

    factsbeta1.php edited online with Bitbucket

--- resources/page/modules/factsbeta1.php
+++ resources/page/modules/factsbeta1.php
@@ -38,7 +38,7 @@ for ($n = 0; $n < count($sentences); ++$n) {
 <div><div class="btn-group btn-group-sm" role="group" aria-label="...">
   <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_343_thumbs_up.png" style="width:18px; height:18px;"> </a><script>  tooltip demo $('.tooltip-test').tooltip() </script></button>
     <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a><script>  tooltip demo $('.tooltip-test').tooltip() </script></button>
- <button type="button" class="btn btn-group-sm btn-default" data-toggle="modal" data-target="#myModal2"  onClick='document.getElementById("<?php echo $i; ?>").src="http://en.wikipedia.org/wiki/<?php echo $subject; ?>";'> <img src="http://beam.la/glyphicons-187-move.png" style="width:18px; height:18px;"></button>
+ <button type="button" class="btn btn-group-sm btn-default";'> <img src="http://beam.la/glyphicons-187-move.png" style="width:18px; height:18px;"></button>
  <!-- Large modal -->
 
 
@@ -48,7 +48,7 @@ for ($n = 0; $n < count($sentences); ++$n) {
     <div class="modal-content" style="height:95%;">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-        <h4 class="modal-title text-left" id="myModalLabel">en.wikipedia.org/<?php echo $subject; ?></h4>
+        <h4 class="modal-title text-left" id="myModalLabel">en.wikipedia.org/<?php echo $subject; data-toggle="modal" data-target="#myModal2"  onClick='document.getElementById("<?php echo $i; ?>").src="http://en.wikipedia.org/wiki/<?php echo $subject; ?>";'> ?></h4>
       </div>
       <div class="modal-body">
         <iframe  id="<?php echo $i; ?>" frameborder="0"  style="text-decoration:none; width:100%; height:75%;"></iframe>
