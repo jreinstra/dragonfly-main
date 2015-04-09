26b12f2eb654e7afe56341569fa535efdd945a65
Jackson Vachal <vacsox@yahoo.com>
2015-04-09 03:59:40 +0000

    testsearch.php edited online with Bitbucket

--- search/testsearch.php
+++ search/testsearch.php
@@ -216,7 +216,7 @@ Videos coming April 20th!
 </div>
 <p> </p>
 
-<div style="width: 10%; padding-right:5px; padding-left:5px;">
+<div style="padding-right:5px; padding-left:150px;">
 
 <input style="padding:10px; background-color: #0DA50F" class="btn btn-success btn-lg" type="submit" value="Save"><a href="/email.php">    </a>
 
