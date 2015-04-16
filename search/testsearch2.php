0bd12fd52204650191c6450589cfa6c261240fdf
Alex Fine <alexkfine2@gmail.com>
2015-04-16 20:14:43 +0000

    restored to previous. Only text is link bug still exists.

--- search/testsearch2.php
+++ search/testsearch2.php
@@ -106,7 +106,7 @@ color:#606060 ;
 <button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal" onClick='document.getElementById("survey").src="https://www.surveymonkey.com/s/PJMKQVL";'>
   Feedback!
 </button>
-<button  type="button" class="btn btn-default"  href="http://dragonflysearch.com/<?php echo $radsearch; ?>">Random!</button>
+<button  type="button" class="btn btn-default"><a   href="http://dragonflysearch.com/<?php echo $radsearch; ?>">Random!</a></button>
 </div>
 <script>$("#myModal").modal("show");
 $("#myModal").css("z-index", "1500");
