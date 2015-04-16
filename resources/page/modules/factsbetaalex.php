04686b5ea12d9256d7b369f8873c1333d58111a7
Alex Fine <alexkfine2@gmail.com>
2015-04-16 19:30:42 +0000

    testing speaking

--- resources/page/modules/factsbetaalex.php
+++ resources/page/modules/factsbetaalex.php
@@ -8,10 +8,12 @@
   <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_343_thumbs_up.png" style="width:18px; height:18px;"> </a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
     <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
  <button type="button" class="btn btn-group-sm btn-default" data-toggle="modal" data-target="#myModal2"  onClick='document.getElementById("<?php echo $i; ?>").src="http://en.wikipedia.org/wiki/<?php echo $subject; ?>";'> <img src="http://beam.la/glyphicons-187-move.png" style="width:18px; height:18px;"></button>
- <button type="button" class="btn btn-group-sm btn-default" > <img src="http://dragonflysearch.com/images/glyphicons-185-volume-up.png" style="width:18px; height:18px;"></button>
+ <button type="button" class="btn btn-group-sm btn-default"   onclick="responsiveVoice.speak($('#text').val(),$('#voiceselection').val());"> <img src="http://dragonflysearch.com/images/glyphicons-185-volume-up.png" style="width:18px; height:18px;"></button>
 
 <!-- Large modal -->
 
+
+ 
 
 <!-- Modal -->
 <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
@@ -38,3 +40,13 @@
 
 
   </blockquote>
+  <script>
+        //Populate voice selection dropdown
+        var voicelist = responsiveVoice.getVoices();
+ 
+        var vselect = $("#voiceselection");
+ 
+        $.each(voicelist, function() {
+                vselect.append($("<option />").val(this.name).text(this.name));
+        });
+</script>
