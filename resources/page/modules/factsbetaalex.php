8235d9c198e880d9c67a6f52d215a2216f75192e
Alex Fine <alexkfine2@gmail.com>
2015-04-18 02:27:51 +0000

    fixing

--- resources/page/modules/factsbetaalex.php
+++ resources/page/modules/factsbetaalex.php
@@ -3,11 +3,11 @@
                              
 
 </p> 
- <a href="http://wikipedia.org/wiki/<?php echo $subject; ?"><small>en.wikipedia.org<cite title="Source Title"></cite></small></a>
+ <small>en.wikipedia.org<cite title="Source Title"></cite></small>
 <div><div class="btn-group btn-group-sm" role="group" aria-label="...">
   <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_343_thumbs_up.png" style="width:18px; height:18px;"> </a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
     <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
- <button type="button" class="btn btn-group-sm btn-default" data-toggle="modal" data-target="#myModal2"  > <img src="http://beam.la/glyphicons-187-move.png" style="width:18px; height:18px;"></button>
+ <button type="button" class="btn btn-group-sm btn-default" data-toggle="modal" data-target="#myModal2"  onClick='document.getElementById("<?php echo $i; ?>").src="http://en.wikipedia.org/wiki/<?php echo $subject; ?>";'> <img src="http://beam.la/glyphicons-187-move.png" style="width:18px; height:18px;"></button>
  <button type="button" class="btn btn-group-sm btn-default"   onclick="responsiveVoice.speak('<?php echo $fact; ?>');"> <img src="http://dragonflysearch.com/images/glyphicons-185-volume-up.png" style="width:18px; height:18px;"></button>
 
 <!-- Large modal -->
@@ -24,7 +24,7 @@
         <h4 class="modal-title text-left" id="myModalLabel">en.wikipedia.org/<?php echo $subject; ?></h4>
       </div>
       <div class="modal-body">
-        EDIT ME
+        <iframe  id="<?php echo $i; ?>" frameborder="0"  style="text-decoration:none; width:100%; height:75%;"></iframe>
       </div>
       <div class="modal-footer">
       </div>
