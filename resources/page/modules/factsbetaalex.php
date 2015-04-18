32e115136178a334686e9ea171da3428e0a99853
Alex Fine <alexkfine2@gmail.com>
2015-04-18 02:33:12 +0000

    TEST 2

--- resources/page/modules/factsbetaalex.php
+++ resources/page/modules/factsbetaalex.php
@@ -3,7 +3,7 @@
                              
 
 </p> 
- <a href="http://en.wikipedia.org/wiki/<?php echo $subject; ?>"><small>en.wikipedia.org<cite title="Source Title"></cite></small></a>
+ <a href="http://en.wikipedia.org/wiki/<?php echo $subject; ?>"  target="_blank"><small>en.wikipedia.org<cite title="Source Title"></cite></small></a>
 <div><div class="btn-group btn-group-sm" role="group" aria-label="...">
   <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_343_thumbs_up.png" style="width:18px; height:18px;"> </a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
     <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
@@ -24,7 +24,7 @@
         <h4 class="modal-title text-left" id="myModalLabel">en.wikipedia.org/<?php echo $subject; ?></h4>
       </div>
       <div class="modal-body">
-        <iframe  id="<?php echo $i; ?>" frameborder="0"  style="text-decoration:none; width:100%; height:75%;"></iframe>
+        EDIT ME
       </div>
       <div class="modal-footer">
       </div>
