811d364df9b370e4030544022e627d1c39ebab6b
Alex Fine <alexkfine2@gmail.com>
2015-04-28 23:12:53 +0000

    continueing to debug. Last one kind of worked.

--- search/testsearch2.php
+++ search/testsearch2.php
@@ -237,13 +237,13 @@ Videos coming April 1st!
    ================================================== -->
    <!-- Placed at the end of the document so the pages load faster -->
   
- <!-- <script src="http://beam.la/npm.js"></script>
+  <script src="http://beam.la/npm.js"></script>
    <script src="http://beam.la/bootstrap.js"></script>
      <script src="http://beam.la/transition.js"></script>
      <script src="http://beam.la/collapse.js"></script>
--->
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
- <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+ <!--<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>-->
  <script src="http://getbootstrap.com/assets/js/docs.min.js"></script>
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
  <!--SCRIPTS-->
