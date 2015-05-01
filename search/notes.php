50fb4f356defd98a42fdec51610ab9aaa1bd2dbc
Alex Fine <alexkfine2@gmail.com>
2015-05-01 19:29:41 +0000

    added in a toolbar + tried to destroy some empty space

--- search/notes.php
+++ search/notes.php
@@ -216,9 +216,26 @@ Videos coming April 1st!
 </tr>
 </tbody></table>
 
+<div role="tabpanel">
 
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+  </ul>
 
-<p> </p>
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="home">...</div>
+    <div role="tabpanel" class="tab-pane" id="profile">...</div>
+    <div role="tabpanel" class="tab-pane" id="messages">...</div>
+    <div role="tabpanel" class="tab-pane" id="settings">...</div>
+  </div>
+
+</div>
+
 <div style="padding-right:5px; padding-left:5px;">
 
 
