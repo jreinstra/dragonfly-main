0268a77c8ba45cfe96d5dd3de9eeb8b60b3ac0d6
Alex Fine <alexkfine2@gmail.com>
2015-05-11 15:37:04 +0000

    factsbetaalex.php edited online with Bitbucket

--- resources/page/modules/factsbetaalex.php
+++ resources/page/modules/factsbetaalex.php
@@ -38,6 +38,26 @@
       </div>
     </div>
   </div>
+   <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="..." alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+   <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="..." alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
 </div>
        
        </div>
