336e4beb28225652271aac561d77cc24f9f59880
Alex Fine <alexkfine2@gmail.com>
2015-05-23 18:54:35 +0000

    testsearchalex.php edited online with Bitbucket

--- search/testsearchalex.php
+++ search/testsearchalex.php
@@ -78,7 +78,7 @@ color:#606060 ;
     <div class="col-xs-10 col-sm-6 col-md-8">
       <a href="/" style="text-decoration:none;"><h1 class="text-center" style=" color: #D92F03; font-size: 40px;">dragonfly - <i>beta</i> </h1></a>
 
-   <form action="/search/" method="GET">
+   <form action="/search/testsearchalex.php" method="GET">
        <div class="input-group">
          <input  name="q" value="<?php echo $searchname; ?>" type="text" class="form-control" placeholder="What are you looking for?">
          <span class="input-group-btn">
