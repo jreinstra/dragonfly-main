8d08e4a77ae59d4871be65b70ccac796d11b67ad
Alex Yancey <alexyancey3@gmail.com>
2015-05-23 17:54:49 -0700

    Worked on EB with Arshia.

--- alexy/eb.py
+++ alexy/eb.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import codecs
+import webbrowser
+
 
 # Pretend to be an iPhone by changing the User-Agent header
 headers = {
@@ -12,6 +15,26 @@ r = requests.get("http://m.eb.com/topic/129392/community-ecology", headers=heade
 #print r.text
 soup = BeautifulSoup(r.text)
 
-for this_div in soup.find_all("div", class_="article"):
-	print(this_div.get_text().encode('ascii', 'ignore'))
+f = codecs.open("eb.html", "w", "utf-8")
 
+for this_div in soup.find_all("div", class_="article"):
+	# Removes table of contents from page
+	for tag in this_div.find_all('div', class_="toc"):
+		tag.replaceWith("")
+
+	# Removes images and other crap from page
+	for tag in this_div.find_all('img'):
+		tag.replaceWith("")
+	for tag in this_div.find_all('div', class_="mag_image"):
+		tag.replaceWith("")
+	for tag in this_div.find_all('a'):
+		tag.replaceWith("")
+	for tag in this_div.find_all('span', class_="caption"):
+		tag.replaceWith("")
+	for tag in this_div.find_all('span', class_="italicize"):
+		tag.replaceWith("")
+
+	f.write(this_div.prettify())
+
+f.close()
+webbrowser.open_new("eb.html")
