c24c8d511c52e497093cf707f7efbd66e0af7b2c
Alex Yancey <alexyancey3@gmail.com>
2015-05-23 18:33:11 -0700

    Finished EB and NYT code.

--- alexy/eb.py
+++ alexy/eb.py
@@ -1,40 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
-import codecs
-import webbrowser
+import re
 
+def get_article_text(id):
+	# Pretend to be an iPhone by changing the User-Agent header
+	headers = {
+	    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
+	}
 
-# Pretend to be an iPhone by changing the User-Agent header
-headers = {
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
-}
+	# Download EB article with fake User-Agent
+	r = requests.get("http://m.eb.com/topic/" + str(id), headers=headers)
 
-# Download EB article with fake User-Agent
-r = requests.get("http://m.eb.com/topic/129392/community-ecology", headers=headers)
+	# Beautify website
+	soup = BeautifulSoup(r.text)
 
-#print r.text
-soup = BeautifulSoup(r.text)
+	for this_div in soup.find_all("div", class_="article"):
+		# Removes table of contents from page
+		for tag in this_div.find_all('div', class_="toc"):
+			tag.replaceWith("")
 
-f = codecs.open("eb.html", "w", "utf-8")
+		# Removes images and other crap from page
+		for tag in this_div.find_all('img'):
+			tag.replaceWith("")
+		for tag in this_div.find_all('div', class_="mag_image"):
+			tag.replaceWith("")
+		for tag in this_div.find_all('a', href=re.compile("/assembly/.*")):
+			tag.replaceWith("")
+		for tag in this_div.find_all('span', class_="caption"):
+			tag.replaceWith("")
+		for tag in this_div.find_all('span', class_="italicize"):
+			tag.replaceWith("")
+		for tag in this_div.find_all('p', style="text-indent:0"):
+			tag.replaceWith("")
 
-for this_div in soup.find_all("div", class_="article"):
-	# Removes table of contents from page
-	for tag in this_div.find_all('div', class_="toc"):
-		tag.replaceWith("")
+		# If we find 'Authors of this article:', assume the article is finished and trim everything after that
+		raw_text = this_div.get_text()
+		last_start = raw_text.find("Authors of this article:") 
+		if not last_start == -1:
+			super_raw_text = raw_text[:(len(raw_text) - last_start)* -1]
+		else:
+			super_raw_text = raw_text
+		return super_raw_text
 
-	# Removes images and other crap from page
-	for tag in this_div.find_all('img'):
-		tag.replaceWith("")
-	for tag in this_div.find_all('div', class_="mag_image"):
-		tag.replaceWith("")
-	for tag in this_div.find_all('a'):
-		tag.replaceWith("")
-	for tag in this_div.find_all('span', class_="caption"):
-		tag.replaceWith("")
-	for tag in this_div.find_all('span', class_="italicize"):
-		tag.replaceWith("")
-
-	f.write(this_div.prettify())
-
-f.close()
-webbrowser.open_new("eb.html")
--- alexy/nyt.py
+++ alexy/nyt.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import codecs
 
 def get_article_text(url):
 	article_text = ""
@@ -17,7 +16,3 @@ def get_article_text(url):
 		article_text += this_div.get_text() + '\n\n'
 
 	return article_text
-
-# f = codecs.open("nyt_text.txt", "w", "utf-8")
-# f.write(get_article_text("http://www.nytimes.com/2015/05/24/world/europe/ireland-gay-marriage-referendum.html?hp&action=click&pgtype=Homepage&module=span-ab-top-region&region=top-news&WT.nav=top-news"))
-# f.close()
