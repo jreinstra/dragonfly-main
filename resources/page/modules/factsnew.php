c65fded9fdcedbba24453f1d3104ea04c655873c
Alex Fine <alexkfine2@gmail.com>
2015-06-04 23:28:36 +0000

    factsnew.php edited online with Bitbucket

--- resources/page/modules/factsnew.php
+++ resources/page/modules/factsnew.php
@@ -3,13 +3,12 @@
 
 
  <p name="text" style="color:white;"><b><?php echo $i; ?>.</b> <?php echo $fact; ?>
-        <input style="width:0px; height:0px; background-color:rgba(255, 255, 255, .7); text-decoration:none;" name="text" value="<?php echo $fact; ?>" >                   
 
 </p> 
  <a href="http://www.britannica.com/search?query=<?php echo $subject; ?>"  target="_blank"><small  style="color:white;">Encyclopedia Britannica<cite title="Source Title"></cite></small></a>
 <div><div class="btn-group btn-group-sm" role="group" aria-label="...">
   <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_343_thumbs_up.png" style="width:18px; height:18px;"> </a></button>
-    <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a></button>
+    <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a>         <input style="width:0px; height:0px; background-color:rgba(255, 255, 255, .7); text-decoration:none;" name="text" value="<?php echo $fact; ?>" > </button>
 <button type="submit" class="btn btn-group-sm btn-default"  > <a href="/storefile.php"><img src="http://beam.la/glyphicons-151-edit.png" style="width:18px; height:18px;"> </a></button>
 </form>
 
