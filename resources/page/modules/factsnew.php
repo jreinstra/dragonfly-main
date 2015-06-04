0016caef5410ebb9f211e6329af60105f4edf0f6
Alex Fine <alexkfine2@gmail.com>
2015-06-04 18:44:31 +0000

    factsnew.php edited online with Bitbucket

--- resources/page/modules/factsnew.php
+++ resources/page/modules/factsnew.php
@@ -3,7 +3,7 @@
                              
 
 </p> 
- <a href="http://www.britannica.com/search?query=<?php echo $subject; ?>"  target="_blank"><small>Encyclopedia Britannica<cite title="Source Title"></cite></small></a>
+ <a href="http://www.britannica.com/search?query=<?php echo $subject; ?>"  target="_blank"><small  style="color:white;">Encyclopedia Britannica<cite title="Source Title"></cite></small></a>
 <div><div class="btn-group btn-group-sm" role="group" aria-label="...">
   <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_343_thumbs_up.png" style="width:18px; height:18px;"> </a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
     <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a><script>  tooltip  $('.tooltip-test').tooltip() </script></button>
