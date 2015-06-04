97eb215a03d252035d97ae327212c677dc99d602
Alex Fine <alexkfine2@gmail.com>
2015-06-04 18:09:48 +0000

    newsearch.php edited online with Bitbucket

--- search/newsearch.php
+++ search/newsearch.php
@@ -75,7 +75,7 @@ color:#606060 ;
 </style></head>
 
  
- <body class="center">
+ <body style="padding-left:15%;">
   <div>
     <div class="col-xs-10 col-sm-6 col-md-8">
       <a href="/" style="text-decoration:none;"><h1 class="text-center" style=" color: #D92F03; font-size: 40px;">dragonfly - <i>beta</i> </h1></a>
