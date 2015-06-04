6e0ae3648b37d9a5bfef07d1eacc501fb60c246a
Alex Fine <alexkfine2@gmail.com>
2015-06-04 23:40:46 +0000

    factsnew.php edited online with Bitbucket

--- resources/page/modules/factsnew.php
+++ resources/page/modules/factsnew.php
@@ -9,20 +9,21 @@
 <div><div class="btn-group btn-group-sm" role="group" aria-label="...">
   <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_343_thumbs_up.png" style="width:18px; height:18px;"> </a></button>
     <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a>         <input style="width:0px; height:0px; background-color:rgba(255, 255, 255, .7); text-decoration:none;" name="text" value="<?php echo $fact; ?>" > </button>
+<script src="http://apis.google.com/js/platform.js"></script>
+<script src="https://apis.google.com/js/platform.js"></script>
+<div class="g-savetodrive" 
+     data-src="http://dragonflysearch.com/myText.txt" 
+     data-filename="Notes" 
+     data-sitename="Dragonfly"> 
 <button type="submit" class="btn btn-group-sm btn-default"  > <a href="/storefile.php"><img src="http://beam.la/glyphicons-151-edit.png" style="width:18px; height:18px;"> </a></button>
+</div>
+
+
 </form>
 
 <button type="button" class="btn btn-group-sm btn-default"   onclick="responsiveVoice.speak('<?php echo $fact; ?>');"> <img src="http://dragonflysearch.com/images/glyphicons-185-volume-up.png" style="width:18px; height:18px;"></button>
 
 <!-- Large modal -->
-
-
-
-
- 
- 
-
-
 
   </blockquote>
   <script>
