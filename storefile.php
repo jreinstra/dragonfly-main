82bfe3c1cdbbf45c88406d280fcaa152cf8c3f28
Alex Fine <alexkfine2@gmail.com>
2015-06-06 19:12:21 +0000

    storefile.php edited online with Bitbucket

--- storefile.php
+++ storefile.php
@@ -1,6 +1,6 @@
 <?php
 $content = $_POST["result"];
-$fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myText.txt" ,"wb");
+$fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myText2.txt" ,"wb");
 fwrite($fp,$content);
 fclose($fp);
 
