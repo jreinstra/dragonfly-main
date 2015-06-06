cce6a72dc357e67fe1f8b627628c9882fa18ece0
Alex Fine <alexkfine2@gmail.com>
2015-06-06 19:08:20 +0000

    newsearch.php edited online with Bitbucket

--- search/newsearch.php
+++ search/newsearch.php
@@ -134,7 +134,6 @@ color:#606060 ;
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
