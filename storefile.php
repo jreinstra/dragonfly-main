f6ff3e1afed470545eba1907693746f8bfe0c1b7
Alex Fine <alexkfine2@gmail.com>
2015-06-06 19:06:14 +0000

    storefile.php edited online with Bitbucket

--- storefile.php
+++ storefile.php
@@ -1,12 +1,7 @@
 <?php
-if (!isset($_SESSION["loop_count"])
-$_SESSION["loop_count"] = 0;
-
 $content = $_POST["result"];
-$fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myText" . $_SESSION["loop_count"] . ".txt" ,"wb");
+$fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myText.txt" ,"wb");
 fwrite($fp,$content);
 fclose($fp);
-
-$_SESSION["loop_count"]++;
 
 ?>
