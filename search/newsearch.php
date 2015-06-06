446808cbedd9cc9a7baa6a1b204e0df494f757e5
Alex Fine <alexkfine2@gmail.com>
2015-06-06 18:55:14 +0000

    newsearch.php edited online with Bitbucket

--- search/newsearch.php
+++ search/newsearch.php
@@ -119,10 +119,18 @@ color:#606060 ;
   <div class="modal-dialog modal-sm">
     <div class="modal-content"><div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Modal title</h4>
+        <h4 class="modal-title">Save</h4>
       </div>
       <div class="modal-body">
-      <a href="/storefile.php">Click Me</a>
+      <h3>To save to Google Docs first click here: </h3>
+      <a href="/storefile.php" target="_blank">Click Me</a>
+      <h3>Then, click here: </h3>
+      <script src="https://apis.google.com/js/platform.js"></script>
+<div class="g-savetodrive" 
+     data-src="http://dragonflysearch.com/myText.txt" 
+     data-filename="Notes" 
+     data-sitename="Dragonfly"> 
+</div>
 
       </div>
       <div class="modal-footer">
