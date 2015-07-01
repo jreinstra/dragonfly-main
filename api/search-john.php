8ea239c9a4832e4bf0b1231179703926c89038d5
yohanstra <jreinstra@gmail.com>
2015-06-30 20:29:02 -0700

    Created test file and added another missing file

---
+++ api/search-john.php
@@ -0,0 +1,89 @@
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"] . "/resources/mysql/connect_eb.php");
+$q = urldecode($_GET["q"]);
+
+$uncut = explode(" ", $q);
+$words = array();
+foreach($uncut as $word) {
+	if(strlen($word) > 0) {
+		$words[] = $word;
+	}
+}
+
+$matches = array();
+foreach($words as $word) {
+	$word = strtolower($word);
+	$sql = "SELECT fact_id, occurrences FROM lexicon WHERE term=? ORDER BY occurrences DESC";
+	$stmt = mysqli_prepare($con, $sql) or die(mysqli_error($con));
+		mysqli_stmt_bind_param($stmt, 's', $word) or die(mysqli_stmt_error($stmt));
+		mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
+		mysqli_stmt_bind_result($stmt, $factID, $factMatches);
+	
+	$empty = true;
+	while(mysqli_stmt_fetch($stmt)) {
+		$empty = false;
+		if(isset($matches[$factID])) {
+			$matches[$factID] = $matches[$factID] + $factMatches;
+		}
+		else {
+			$matches[$factID] = $factMatches;
+		}
+	}
+	mysqli_stmt_close($stmt);
+	
+	if($empty) {
+		$q = strtolower($word);
+		if(substr($q, 0, 1) != " ") $q = " " . $q;
+		if(substr($q, -1) != " ") $q = $q . " ";
+		$sql = 'SELECT fact_id, fact FROM eb.facts WHERE LOWER(fact) LIKE "%?%"';
+		$stmt = mysqli_prepare($con, $sql) or die(mysqli_error($con));
+			mysqli_stmt_bind_param($stmt, 's', $q) or die(mysqli_stmt_error($stmt));
+			mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
+			mysqli_stmt_bind_result($stmt, $factID, $factText);
+		
+		$newFacts = array();
+		while(mysqli_stmt_fetch($stmt)) {
+			$newFacts[] = array("ID"=>$factID, "Text"=>$factText);
+		}
+		mysqli_stmt_close($stmt);
+		
+		foreach($newFacts as $newFact) {
+			$newFactMatches = substr_count($newFact["Text"], $q);
+			$sql = 'INSERT INTO lexicon (term, fact_id, occurrences) VALUES (?, ?, ?)';
+			$stmt = mysqli_prepare($con, $sql) or die(mysqli_error($con));
+				mysqli_stmt_bind_param($stmt, 'sii', $word, $newFact["ID"], $newFactMatches) or die(mysqli_stmt_error($stmt));
+				mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
+				mysqli_stmt_close($stmt);
+			if(!isset($matches[$newFact["ID"]])) $matches[$newFact["ID"]] = 0;
+			$matches[$newFact["ID"]] = $matches[$newFact["ID"]] + $newFactMatches;
+		}
+	}
+}
+
+//$subject = getSubject($q);
+if(substr($q, 0, 1) != " ") $q = " " . $q;
+if(substr($q, -1) != " ") $q = $q . " ";
+
+$sql = 'SELECT fact FROM eb.facts WHERE LOWER(fact) LIKE "%' . strtolower($q) . '%"';
+
+$stmt = mysqli_prepare($con, $sql) or die(mysqli_error($con));
+	mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
+	mysqli_stmt_bind_result($stmt, $text);
+
+$facts = array();
+while(mysqli_stmt_fetch($stmt)) {
+	$facts[] = $text;
+}
+
+
+
+while($row=mysql_fetch_array($result)){
+      array_push($facts,$row['fact']);
+      echo $row['fact'];
+}
+
+$result = array("Matches"=>$matches, "Subject"=>$q, "Facts"=>$facts);
+//echo "<pre>"; print_r($result); echo "</pre>";
+echo json_encode($result);
+
+?>
---
+++ resources/mysql/connect_eb.php
@@ -0,0 +1,24 @@
+<?php
+
+//old hosted database settings
+/*$dbhost = localhost;
+$dbuser = "root";//beam_root
+$dbpass = "BEAM123";//Rx4sS4KJwDVEMGZc
+$dbname = "beam";*/
+
+//i)8aiYsbXHeGxox[P(TdLZkG*BJm9LJGzZD9tkbWmC8%y2X^tt
+
+$dbhost = localhost;
+$dbuser = "beam";//beam_root
+$dbpass = "i)8aiYsbXHeGxox[P(TdLZkG*BJm9LJGzZD9tkbWmC8%y2X^tt";//Rx4sS4KJwDVEMGZc
+$dbname = "eb";
+
+$con = mysqli_connect($dbhost, $dbuser, $dbpass);
+if(mysqli_connect_errno($con)) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+else {
+	mysqli_select_db($con, $dbname);
+}
+
+?>
