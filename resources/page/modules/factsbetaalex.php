0427b30c6ada5cdab27680a3ee3b176bd22c475d
Alex Fine <alexkfine2@gmail.com>
2015-08-20 17:49:20 +0000

    coloring

--- resources/page/modules/factsbetaalex.php
+++ resources/page/modules/factsbetaalex.php
@@ -10,12 +10,11 @@ document.getElementById("text").value += '<?php echo $fact; ?>' + 'http://www.br
  <p name="text" style="color:#484848;"><b><?php echo $i; ?>.</b> <?php echo $fact; ?>
 
 </p>
- <a href="http://www.britannica.com/search?query=<?php echo $subject; ?>"  target="_blank"><small  style="color:white;">Encyclopedia Britannica<cite title="Source Title"></cite></small></a>
+ <a href="http://www.britannica.com/search?query=<?php echo $subject; ?>"  target="_blank"><small>Encyclopedia Britannica<cite title="Source Title"></cite></small></a>
 <div><div class="btn-group btn-group-sm" role="group" aria-label="...">
   <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_343_thumbs_up.png" style="width:18px; height:18px;"> </a></button>
     <button type="button" class="btn btn-group-sm btn-default"> <a href="#" rel="tooltip" title="" class="tooltip-test" data-original-title="Did this fact address your search?"><img src="http://beemsearch.com/glyphicons_344_thumbs_down.png" style="width:18px; height:18px;"></a>         <input style="width:0px; height:0px; background-color:rgba(255, 255, 255, .7); text-decoration:none;" name="text" value="<?php echo $fact; ?>" > </button>
-<script src="http://apis.google.com/js/platform.js"></script>
-<script src="https://apis.google.com/js/platform.js"></script>
+
 <!--<div class="g-savetodrive"
      data-src="http://dragonflysearch.com/myText.txt"
      data-filename="Notes"
